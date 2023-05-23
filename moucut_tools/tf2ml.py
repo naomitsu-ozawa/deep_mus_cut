7906e9941981ea7f305a46b2f6306681dd7db426
naomitsu_ozawa <ozawa_naomitsu@kurume-u.ac.jp>
2023-05-23 11:54:35 +0900

    added tf2ml

---
+++ moucut_tools/tf2ml.py
@@ -0,0 +1,16 @@
+# %%
+from pathlib import Path
+
+import coremltools as ct
+import tensorflow as tf
+
+
+# %%
+def tf2ml(input_path):
+    print("\033[32mTensorFlowモデルをCoreMLモデルへ変換します。\033[0m")
+    in_path = Path(input_path)
+    out_path = f"{in_path.parent}/{in_path.stem}.mlmodel"
+    print(out_path)
+    tf_model = tf.keras.models.load_model(input_path)
+    ct_model = ct.convert(tf_model)
+    ct_model.save(out_path)
