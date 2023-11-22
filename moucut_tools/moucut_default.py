ea995c50f2fe5ef3fa325b092f548340e4ebb89c
naomitsu-ozawa <ozawa_naomitsu@kurume-u.ac.jp>
2023-11-22 14:47:55 +0900

    update

--- moucut_tools/moucut_default.py
+++ moucut_tools/moucut_default.py
@@ -178,7 +178,7 @@ def moucut(
                                 cnn_result = round(float(cnn_result), 4)
                                 cnn_bar = int(cnn_result * 139 + 101)
 
-                            if cnn_result > 0.95:
+                            if cnn_result > 0.9:
                                 for_kmeans_array.append(croped)
                                 count += 1
                                 pip_croped = croped
